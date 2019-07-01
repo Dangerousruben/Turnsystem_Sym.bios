21315b8f35d8af796a1828eb6cc5d92e990f190c
Dangerousruben <Ruben079@live.nl>
2019-07-01 12:46:08 +0200

    Create LinkedListNode.cpp

---
+++ source/private/LinkedListNode.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LinkedListNode.h"
+
+//Constructor
+ULinkedListNode::ULinkedListNode()
+{
+
+}
+
+//Destructor
+ULinkedListNode::~ULinkedListNode()
+{
+
+}
