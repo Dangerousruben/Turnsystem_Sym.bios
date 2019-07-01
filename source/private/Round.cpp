e7b14b41d7a9f957c5860e05e9f87d1e3811c630
Dangerousruben <Ruben079@live.nl>
2019-07-01 13:01:13 +0200

    Create Round.cpp

---
+++ source/private/Round.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Round.h"
+#include "UnitBase.h"
+
+//Constructor
+URound::URound()
+{
+}
+
+//Destructor
+URound::~URound()
+{
+}
+
+//Sorts the round based on RoundInitiative in descending order
+void URound::SortRound()
+{
+	//Sorts the array on the key values in descending order
+	TurnOrder.Sort([](FUnitRoundInfo A, FUnitRoundInfo B) 
+	{
+		return A.RoundInitiative > B.RoundInitiative;
+	});
+}
+
+//Removes the unit from the map
+//@param[in] a_Unit : The unit that will be removed
+void URound::RemoveUnitFromTurnOrder(AUnitBase* a_Unit)
+{
+	for (auto &iter : TurnOrder)
+	{
+		//Checks if the map value is equal to the unit reference given
+		if (iter.UnitRef == a_Unit)
+		{
+			TurnOrderMap.(*iter);
+			SortRound();
+			return;
+		}
+	}
+}
